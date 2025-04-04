286db1051c755256be3157c4093eaff3182596cf
karloshenrique <141341177+karloshenrique@users.noreply.github.com>
2025-04-04 15:20:18 +0000

    uyt

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(){
+   const aumentaFonteBotao=document.getElementById("aumentar-fonte");
+   
+   let tamanhoAtualfonte=1;
+
+   aumentaFonteBotao.addEventListener('click',function({
+   tamanhoAtualfonte +=0,1;
+
+   }))
+   
+
+})
